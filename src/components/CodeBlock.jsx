1db99efcd8e891565406feccd61e6ac20395b4b1
thatanjan <anjan.cules@gmail.com>
2021-06-12 09:42:10 +0600

    code block overflow will be scrolled

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -13,7 +13,10 @@ const CodeBlock = ({ children, className }) => {
 			language={language}
 		>
 			{({ className, style, tokens, getLineProps, getTokenProps }) => (
-				<pre className={className} style={{ ...style, padding: '20px' }}>
+				<pre
+					className={className}
+					style={{ ...style, padding: '20px', overflowX: 'scroll' }}
+				>
 					{tokens.map((line, i) => (
 						<div key={i} {...getLineProps({ line, key: i })}>
 							{line.map((token, key) => (
